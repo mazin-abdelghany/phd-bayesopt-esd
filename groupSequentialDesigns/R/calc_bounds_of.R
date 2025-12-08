25035e471cb36f9d386bad8eabc30fd704ce00e3
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2025-12-08 11:50:41 -0800

    Completed O'Brien-Fleming boundary documentation

--- groupSequentialDesigns/R/calc_bounds_of.R
+++ groupSequentialDesigns/R/calc_bounds_of.R
@@ -1,14 +1,35 @@
-#' Title
+#' Calculate O'Brien-Fleming bounds for a group sequential clinical trial
 #'
-#' @param n_analyses
-#' @param alpha
-#' @param n_patients
-#' @param sided
+#' `calc_bounds_of` returns one- or two-sided O'Brien-Fleming boundary values
+#' for a group sequential clinical trial design at a specified \eqn{\alpha} and
+#' sample size using interval bisection.
 #'
-#' @returns
+#' @param n_analyses Integer indicating the number of interim analyses
+#' that will be performed in the group sequential design.
+#' @param alpha Type I error rate to be targeted when calculating the bounds.
+#' @param n_patients Vector of integers indicating the sample size (per group)
+#' at each of the interim analyses. Note that the length of this vector must
+#' correspond to the number of analyses `n_analyses`.
+#' @param sided A string "one.sided" or "two.sided" indicating whether the
+#' function returns one- or two-sided bounds. In one-sided bounds, the boundary
+#' values are equal at the last analysis.
+#'
+#' @returns A list of length 8 that contains the final upper and lower
+#' O'Brien-Fleming bounds calculated and the last simulation run. The simulation
+#' output is from `gsd_simulations` and shows the probability of stopping for
+#' futility and efficacy under both the null and alternative hypotheses
+#' specified. It also calculates the type I error (\eqn{\alpha}), the power
+#' (\eqn{1 - \beta}) where \eqn{\beta} is the type II error, the expected sample
+#' size, and the variance of the sample size.
 #' @export
 #'
+#' @seealso [gsd_simulations()] for details on the simulation return.
+#'
 #' @examples
+#' calc_bounds_of(n_analyses = 3,
+#'                alpha = 0.05,
+#'                n_patients = c(20, 40, 60),
+#'                sided = "one.sided")
 
 calc_bounds_of <- function(n_analyses = 3,
                            alpha = 0.05,
